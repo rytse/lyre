a4b7d021e12d8fee1a07180b3ee33879c99fe52a
matthewjcox <matthewjcox.2000@gmail.com>
2019-09-14 21:36:15 -0400

    Flask test/demo code

--- app.py
+++ app.py
@@ -1,2 +1,23 @@
-import back.backend
-import front.frontend
+# print("start")
+# # if 1:
+# #     raise ValueError
+# import back.backend
+# print("back done")
+# import front.frontend
+# print("front done")
+
+from flask import Flask, escape, request, render_template
+import random
+
+app = Flask(__name__)
+
+@app.route('/')
+def mainpage():
+    return render_template('flask_test.html')
+
+@app.route('/update/', methods=['GET'])
+def hello():
+    return f'Hello! {random.random()}'
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, debug=True)
