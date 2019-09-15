9f8de92d6c0ad64e7f74bebd9a7d9e2aaceb6170
Ryan Tse <rytse@umd.edu>
2019-09-15 01:07:45 -0400

    Start over

--- back/backend.py
+++
@@ -1,9 +0,0 @@
-from ibm_watson import NaturalLanguageUnderstandingV1
-
-print('Back-end started!')
-import time
-while(1):
-    time.sleep(100)
-    with open("be.txt",'w') as f:
-        f.write('Back-end running!\n')
-    print('foo')
--- front/frontend.py
+++
@@ -1,6 +0,0 @@
-print('Front-end started!')
-import time
-while(1):
-    time.sleep(100)
-    with open("fe.txt",'w') as f:
-        f.write('Front-end running!\n')
